c1c7c893ae3efd34d650313606597d34e36f8eb5
Harshit Gangwar <harshitgangwar9918@gmail.com>
2026-08-05 21:35:35 +0530

    Add files via upload

---
+++ solutions/Medium-LeetCode/152. Maximum Product Subarray.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int maxProduct(vector<int> &nums)
+    {
+        int best_end = nums[0];
+        int min_end = nums[0];
+        int max_end = nums[0];
+        int ans = nums[0];
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            int v1 = nums[i];
+            int v2 = min_end * nums[i];
+            int v3 = max_end * nums[i];
+
+            min_end = min(v1, min(v2, v3));
+            max_end = max(v1, max(v2, v3));
+            ans = max(ans, max(max_end, min_end));
+        }
+        return ans;
+    }
+};
